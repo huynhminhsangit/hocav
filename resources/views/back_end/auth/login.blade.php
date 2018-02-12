9bc2686e8bac90ee9567c65fdf67dd7afcb8f696
whatsalem <zolekwhatsalem@gmail.com>
2018-02-12 23:59:58 +0700

    dsds

--- resources/views/back_end/auth/login.blade.php
+++ resources/views/back_end/auth/login.blade.php
@@ -7,10 +7,10 @@
             <h5 class="card-header text-center">ĐĂNG NHẬP</h5>
             <div class="card-body">
 
-                <form class="form-horizontal" method="POST" action="{{ route('login') }}">
+                <form class="form-horizontal" method="POST" action="{{ route('login') }}" novalidate>
                         {{ csrf_field() }}
 
-                        <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
+                        <div class="form-group">
                             <label for="email" class="col-md-4 control-label">EMAIL</label>
 
                                 <input id="email" type="email" class="form-control" name="email" value="{{ old('email') }}" required autofocus>
@@ -22,7 +22,7 @@
                                 @endif
                         </div>
 
-                        <div class="form-group{{ $errors->has('password') ? ' has-error' : '' }}">
+                        <div class="form-group">
                             <label for="password" class="col-md-4 control-label">MẬT KHẨU</label>
 
                                 <input id="password" type="password" class="form-control" name="password" required>
