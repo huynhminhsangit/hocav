f8c7426b393d0316c72bedaa907497a952cbd936
whatsalem <zolekwhatsalem@gmail.com>
2018-02-18 21:26:31 +0700

    sddsd

--- resources/views/back_end/layout/master.blade.php
+++ resources/views/back_end/layout/master.blade.php
@@ -69,12 +69,8 @@
           </span>
         </li>
         <li class="nav-item text-warning" data-toggle="tooltip" data-placement="right">
-          <span class="nav-link-text">Địa chỉ IP của bạn :
-            @if(!Auth::user()->last_login_ip)
-            0
-            @else
-            {{Auth::user()->last_login_ip}}
-            @endif
+          <span class="nav-link-text">Địa chỉ IP của bạn :{{Auth::user()->last_login_ip}}
+            
           </span>
         </li><li class="nav-item text-warning" data-toggle="tooltip" data-placement="right">
           <span class="nav-link-text">Lần cuối đăng nhập :
