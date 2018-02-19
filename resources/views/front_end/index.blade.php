ef078fece697c8263f2856002cb91865bd0efa9b
whatsalem <zolekwhatsalem@gmail.com>
2018-02-19 10:20:18 +0700

    sddsd

--- resources/views/front_end/index.blade.php
+++ resources/views/front_end/index.blade.php
@@ -1,12 +1,8 @@
-﻿
-</div>
-
-<!DOCTYPE html>
+﻿<!DOCTYPE html>
 <html lang="en" ng-app="myapp">
-@include('back_end.layout.head')
 <body>
-<div ng-controller="BannerController" ng-init="showbanner()">
-  <img ng-src="<%'upload/banner/' + showbanner.image%>"/>
-    @include('back_end.layout.js')
+    <div ng-controller="BannerController" ng-init="showbanner()">
+      <img ng-src="<%'upload/banner/' + showbanner.image%>"/>
+      @include('back_end.layout.js')
   </body>
   </html>
