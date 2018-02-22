b9618598d230b59c429a54a3644b690bd7f2d439
whatsalem <zolekwhatsalem@gmail.com>
2018-02-22 18:50:19 +0700

    dsddsd

--- config/app.php
+++ config/app.php
@@ -52,7 +52,7 @@ return [
     |
     */
 
-    'url' => env('APP_URL', 'https://hocav.herokuapp.com'),
+    'url' => env('APP_URL', 'https://avhiepgays.tk'),
 
     /*
     |--------------------------------------------------------------------------
