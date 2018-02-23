63be79c50c4fefe80f444d536e091df9d18c0333
whatsalem <zolekwhatsalem@gmail.com>
2018-02-23 19:16:44 +0700

    sdsdd

--- public/back_end/js/web.js
+++ public/back_end/js/web.js
@@ -1,16 +1,3 @@
 $("#logout").click (function () {
 			$("#logout-form").submit();
         });
-$("#act").click (function () {
-        	$("#descrip").prop("readonly",false);
-        		$("#descrip1").prop("readonly",false);
-                $("#addButton").show();
-                $("#imag").show();
-                
-        });
-        $("#addButton").click (function () {
-            $("#descrip").prop("readonly",true);
-                $("#descrip1").prop("readonly",true);
-                $("#addButton").hide();
-                $("#imag").hide();
-        });
